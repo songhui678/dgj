ecf1dce60f10b70f2a3a19a7f35eaa0cf7ee9f0a
songhui678 <songhui678@gmail.com>
2017-12-24 18:05:44 +0800

    ss

--- backend/web/git-webhook-handler.php
+++ backend/web/git-webhook-handler.php
@@ -9,7 +9,7 @@ if (empty($requestBody)) {
 $content = json_decode($requestBody, true);
 //若是主分支且提交数大于0
 if ($content['ref'] == 'refs/heads/master' && $content['total_commits_count'] > 0) {
-	$res = shell_exec("cd {$path} && git pull 2>&1"); //以www用户运行
+	$res = shell_exec("cd {$path} && git pull origin master 2>&1"); //以www用户运行
 	$res_log = '-------------------------' . PHP_EOL;
 	$res_log .= $content['user_name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . $content['total_commits_count'] . '个commit：' . PHP_EOL;
 	$res_log .= $res . PHP_EOL;
