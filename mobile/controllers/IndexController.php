d096d4cf059d9cd903473ee72e83edf9682be9f5
songhui678 <songhui678@gmail.com>
2018-01-10 22:22:27 +0800

    666

--- mobile/controllers/IndexController.php
+++ mobile/controllers/IndexController.php
@@ -19,7 +19,7 @@ class IndexController extends Controller {
 		$this->getView()->title = "冻干机|进口冻干机|实验室冻干机|原位冻干机|纳米材料冻干机|制药冻干机|中试冻干机|生物制品冻干机-新默真科技（北京）有限公司";
 		$this->getView()->metaTags['keywords'] = '美国VirTis冻干机，冷冻干燥机，超微粉气流粉碎机，微射流均质机，加拿大Simport耗材';
 		$this->getView()->metaTags['description'] = '进口冻干机 美国VirTis冻干机-新默真科技，为您提供美国SP SCIENTIFIC公司生产的最佳配置的冷冻干燥设备，包括实验型冻干机、中试型冻干机、小型生产型及产业型冻干机；协助您选择最高性能的酶标仪；为您推荐最稳定的蠕动泵和灌装机！新默真科技，为您提供冻干机、酶标仪、蠕动泵和灌装机等产品专业的技术咨询和服务。';
-		$cateList = GoodsCat::find()->where(array('pid' => 0, "status" => 1))->orderBy('sort asc')->limit(5)->asArray()->all();
+		$cateList = GoodsCat::find()->where(array('pid' => 0, "status" => 1))->orderBy('sort asc')->limit(12)->asArray()->all();
 		$adCate = AdCat::find()->where(array("name" => 'index', "status" => 1))->one();
 		$adverList = array();
 		if (!empty($adCate)) {
